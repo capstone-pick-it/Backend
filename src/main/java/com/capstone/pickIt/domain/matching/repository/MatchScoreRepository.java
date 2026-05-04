ce792bd7184f2d9c61df5f293574c6a9d6ce839a
romdy <kimjh0937@gmail.com>
2026-05-05 05:08:37 +0900

    fix: MatchScoreRepository class를 interface로 수정

--- src/main/java/com/capstone/pickIt/domain/matching/repository/MatchScoreRepository.java
+++ src/main/java/com/capstone/pickIt/domain/matching/repository/MatchScoreRepository.java
@@ -1,4 +1,7 @@
 package com.capstone.pickIt.domain.matching.repository;
 
-public class MatchScoreRepository {
+import com.capstone.pickIt.domain.matching.entity.MatchScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchScoreRepository extends JpaRepository<MatchScore, Long> {
 }
