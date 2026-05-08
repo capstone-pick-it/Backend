1bbf16bd1b1e0f3ebedd0d105101e92ec19fbc38
ownue <enu_i@naver.com>
2026-05-08 13:26:28 +0900

    chore: revieweeUserId를 양수만 받도록 수정

--- src/main/java/com/capstone/pickIt/api/project/dto/request/PeerReviewCreateRequestDTO.java
+++ src/main/java/com/capstone/pickIt/api/project/dto/request/PeerReviewCreateRequestDTO.java
@@ -3,9 +3,11 @@ package com.capstone.pickIt.api.project.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record PeerReviewCreateRequestDTO(
         @NotNull
+        @Positive
         Long revieweeUserId,
 
         @NotNull
