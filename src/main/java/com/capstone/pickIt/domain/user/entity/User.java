79f6c96e1cf63d4ed0bf00c11de2a7f7e9c9b221
wonee1 <gimchaewon417@gmail.com>
2026-05-14 14:37:20 +0900

    refactor : 온보딩 상태 및 기본 정보 엔티티 컬럼 추가

--- src/main/java/com/capstone/pickIt/domain/user/entity/User.java
+++ src/main/java/com/capstone/pickIt/domain/user/entity/User.java
@@ -4,6 +4,9 @@ import com.capstone.pickIt.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -26,6 +29,31 @@ public class User extends BaseEntity {
     @Column(name = "nickname", length = 50, nullable = false)
     private String nickname;
 
+    //기본 정보 엔티티 추가
+    @Column(name = "school", length = 100)
+    private String school;
+
     @Column(name = "major", length = 100)
     private String major;
+
+    @Column(name = "grade")
+    private Integer grade;
+
+    @Column(name = "semester", length = 20)
+    private String semester;
+
+    @Builder.Default
+    @ElementCollection
+    @CollectionTable(name = "user_courses", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "course", length = 100)
+    private List<String> courses = new ArrayList<>();
+
+    //온보딩 엔티티 추가
+    @Builder.Default
+    @Column(name = "onboarding_completed", nullable = false)
+    private boolean onboardingCompleted = false;
+
+    @Builder.Default
+    @Column(name = "onboarding_step", nullable = false)
+    private int onboardingStep = 1;
 }
