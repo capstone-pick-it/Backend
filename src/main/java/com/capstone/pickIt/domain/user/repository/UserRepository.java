7c56e966076d380632eb942d6ef936b3aaaa6bdd
wonee1 <gimchaewon417@gmail.com>
2026-05-16 14:41:18 +0900

    feat : 탈퇴 후 30일 지난 사용자 조회

--- src/main/java/com/capstone/pickIt/domain/user/repository/UserRepository.java
+++ src/main/java/com/capstone/pickIt/domain/user/repository/UserRepository.java
@@ -3,9 +3,16 @@ package com.capstone.pickIt.domain.user.repository;
 import com.capstone.pickIt.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
-    java.util.Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    // 탈퇴 후 30일 지난 사용자 조회 (하드 딜리트용)
+    List<User> findByDeletedAtBeforeAndDeletedAtIsNotNull(LocalDateTime dateTime);
 }
