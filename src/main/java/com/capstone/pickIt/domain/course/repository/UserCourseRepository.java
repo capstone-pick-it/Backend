25c65bf4f62d71c9ab36fb1930ae81cb95132d8a
dppfls <a93864140@gmail.com>
2026-05-17 23:44:13 +0900

    feat: 팀원 요청 보내기 Repostiory 작성

--- src/main/java/com/capstone/pickIt/domain/course/repository/UserCourseRepository.java
+++ src/main/java/com/capstone/pickIt/domain/course/repository/UserCourseRepository.java
@@ -2,8 +2,29 @@ package com.capstone.pickIt.domain.course.repository;
 
 import com.capstone.pickIt.domain.course.entity.UserCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserCourseRepository extends JpaRepository<UserCourse, Long> {
 
     boolean existsByUserIdAndCourseId(Long userId, Long courseId);
+
+    @Query("""
+            SELECT COUNT(uc)
+            FROM UserCourse uc
+            WHERE uc.course.id = :courseId
+              AND uc.user.id IN (:userId1, :userId2)
+            """)
+    long countUsersByCourse(
+            Long userId1,
+            Long userId2,
+            Long courseId
+    );
+
+    default boolean existsCommonCourse(
+            Long userId1,
+            Long userId2,
+            Long courseId
+    ) {
+        return countUsersByCourse(userId1, userId2, courseId) == 2;
+    }
 }
---
+++ src/main/java/com/capstone/pickIt/domain/project/repository/TeamRequestRepository.java
@@ -0,0 +1,26 @@
+package com.capstone.pickIt.domain.project.repository;
+
+import com.capstone.pickIt.domain.project.entity.TeamRequest;
+import com.capstone.pickIt.domain.project.entity.TeamRequestStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRequestRepository extends JpaRepository<TeamRequest, Long> {
+
+    boolean existsBySenderIdAndCourseIdAndTeamRequestStatus(
+            Long senderId,
+            Long courseId,
+            TeamRequestStatus teamRequestStatus
+    );
+
+    boolean existsBySenderIdAndReceiverIdAndTeamRequestStatus(
+            Long senderId,
+            Long receiverId,
+            TeamRequestStatus teamRequestStatus
+    );
+
+    boolean existsByChatRoomIdAndReceiverIdAndTeamRequestStatus(
+            Long chatRoomId,
+            Long receiverId,
+            TeamRequestStatus teamRequestStatus
+    );
+}
