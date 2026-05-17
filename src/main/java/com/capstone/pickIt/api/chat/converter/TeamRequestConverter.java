583f82bc9d184ed97d08f9c0537576729918556e
dppfls <a93864140@gmail.com>
2026-05-17 23:35:19 +0900

    feat: 팀원 요청 보내기 Converter 작성

---
+++ src/main/java/com/capstone/pickIt/api/chat/converter/TeamRequestConverter.java
@@ -0,0 +1,26 @@
+package com.capstone.pickIt.api.chat.converter;
+
+import com.capstone.pickIt.api.chat.dto.response.TeamRequestResponseDTO;
+import com.capstone.pickIt.domain.project.entity.TeamRequest;
+
+public class TeamRequestConverter {
+
+    private TeamRequestConverter() {
+    }
+
+    public static TeamRequestResponseDTO.Create toCreateResponse(TeamRequest teamRequest) {
+        return new TeamRequestResponseDTO.Create(
+                teamRequest.getId(),
+                teamRequest.getChatRoom().getId(),
+                new TeamRequestResponseDTO.CourseInfo(
+                        teamRequest.getCourse().getId(),
+                        teamRequest.getCourse().getCourseName()
+                ),
+                teamRequest.getSender().getId(),
+                teamRequest.getReceiver().getId(),
+                teamRequest.getTeamRequestStatus().name(),
+                "SENDER",
+                teamRequest.getCreatedAt()
+        );
+    }
+}
