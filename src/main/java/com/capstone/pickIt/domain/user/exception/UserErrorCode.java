3e194c56d319a7228990b169ee96d5ec86fe4dfc
wonee1 <gimchaewon417@gmail.com>
2026-05-19 23:27:53 +0900

    feat: 비밀번호 재설정 에러 코드 추가 및 User 엔티티 updatePassword 메서드 추가

--- src/main/java/com/capstone/pickIt/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/capstone/pickIt/domain/user/exception/UserErrorCode.java
@@ -12,6 +12,8 @@ public enum UserErrorCode implements BaseCode {
     USER_EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "USER400_1", "이메일 인증이 완료되지 않은 사용자입니다."),
     EMAIL_CODE_INVALID(HttpStatus.BAD_REQUEST, "USER400_2", "인증 코드가 올바르지 않거나 만료되었습니다."),
     EMAIL_DOMAIN_INVALID(HttpStatus.BAD_REQUEST, "USER400_3", "학교 이메일(@ac.kr)만 사용 가능합니다."),
+    PASSWORD_RESET_CODE_INVALID(HttpStatus.BAD_REQUEST, "USER400_4", "비밀번호 재설정 인증 코드가 올바르지 않거나 만료되었습니다."),
+    PASSWORD_RESET_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "USER400_5", "비밀번호 재설정 인증이 완료되지 않았습니다."),
 
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "USER401_1", "비밀번호가 올바르지 않습니다."),
     AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "USER401_2", "이메일 또는 비밀번호가 올바르지 않습니다."),
