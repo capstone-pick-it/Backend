75da523196fb0db512d74849f9528c2490a85fca
romdy <kimjh0937@gmail.com>
2026-05-20 15:50:33 +0900

    feat: UserCourseProfileRepository 추가 및 UserCourseProfile에서 updateImportanceLevel메서드 추가

--- src/main/java/com/capstone/pickIt/domain/course/repository/UserCourseProfileRepository.java
+++ src/main/java/com/capstone/pickIt/domain/course/repository/UserCourseProfileRepository.java
@@ -3,9 +3,14 @@ package com.capstone.pickIt.domain.course.repository;
 import com.capstone.pickIt.domain.course.entity.UserCourseProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserCourseProfileRepository extends JpaRepository<UserCourseProfile, Long> {
 
+    List<UserCourseProfile> findByUserIdAndDeletedAtIsNull(Long userId);
+
     Optional<UserCourseProfile> findByUserIdAndCourseId(Long userId, Long courseId);
+
+    boolean existsByUserIdAndCourseId(Long userId, Long courseId);
 }
