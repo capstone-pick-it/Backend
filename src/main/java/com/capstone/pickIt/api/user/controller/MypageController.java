99a0bb1428844bea40a27ede9d96f378d49887ed
wonee1 <gimchaewon417@gmail.com>
2026-05-22 21:53:01 +0900

    feat : 강의 추가 컨트롤러 구현

--- src/main/java/com/capstone/pickIt/api/user/controller/MypageController.java
+++ src/main/java/com/capstone/pickIt/api/user/controller/MypageController.java
@@ -1,5 +1,6 @@
 package com.capstone.pickIt.api.user.controller;
 
+import com.capstone.pickIt.api.user.dto.request.AddCourseRequestDTO;
 import com.capstone.pickIt.api.user.dto.response.CourseCardResponseDTO;
 import com.capstone.pickIt.api.user.service.MypageCourseService;
 import com.capstone.pickIt.global.apiPayload.response.ApiResponse;
@@ -7,6 +8,7 @@ import com.capstone.pickIt.global.apiPayload.response.SuccessCode;
 import com.capstone.pickIt.global.config.security.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +27,13 @@ public class MypageController {
     public ApiResponse<List<CourseCardResponseDTO>> getCourseCards() {
         Long userId = SecurityUtil.requireUserId();
         return ApiResponse.onSuccess(SuccessCode.OK, mypageCourseService.getCourseCards(userId));
+    }
+
+    @Operation(summary = "강의 추가", description = "마이페이지에서 강의를 추가합니다.")
+    @PostMapping("/me/courses")
+    public ApiResponse<String> addCourse(@RequestBody @Valid AddCourseRequestDTO request) {
+        Long userId = SecurityUtil.requireUserId();
+        mypageCourseService.addCourse(userId, request);
+        return ApiResponse.onSuccess(SuccessCode.OK, "강의 추가가 완료되었습니다.");
     }
 }
