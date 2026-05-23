76f7742072fcbdc176baa03426ce251c12bec683
wonee1 <gimchaewon417@gmail.com>
2026-05-23 18:56:00 +0900

    fix : point 0으로 초기화

--- src/main/java/com/capstone/pickIt/api/user/service/MypageProfileServiceImpl.java
+++ src/main/java/com/capstone/pickIt/api/user/service/MypageProfileServiceImpl.java
@@ -1,13 +1,7 @@
 package com.capstone.pickIt.api.user.service;
 
 import com.capstone.pickIt.api.user.dto.response.MyPageProfileResponseDTO;
-import com.capstone.pickIt.domain.point.entity.Point;
-import com.capstone.pickIt.domain.point.exception.PointErrorCode;
-import com.capstone.pickIt.domain.point.exception.PointException;
 import com.capstone.pickIt.domain.point.repository.PointRepository;
-import com.capstone.pickIt.domain.teamlevel.entity.TeamLevel;
-import com.capstone.pickIt.domain.teamlevel.exception.TeamLevelErrorCode;
-import com.capstone.pickIt.domain.teamlevel.exception.TeamLevelException;
 import com.capstone.pickIt.domain.teamlevel.repository.TeamLevelRepository;
 import com.capstone.pickIt.domain.user.entity.User;
 import com.capstone.pickIt.domain.user.exception.UserErrorCode;
@@ -31,18 +25,20 @@ public class MypageProfileServiceImpl implements MypageProfileService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
 
-        Point point = pointRepository.findByUserId(userId)
-                .orElseThrow(() -> new PointException(PointErrorCode.POINT_NOT_FOUND));
+        int point = pointRepository.findByUserId(userId)
+                .map(p -> p.getBalance())
+                .orElse(0);
 
-        TeamLevel teamLevel = teamLevelRepository.findByUserId(userId)
-                .orElseThrow(() -> new TeamLevelException(TeamLevelErrorCode.TEAM_LEVEL_NOT_FOUND));
+        int teamLevel = teamLevelRepository.findByUserId(userId)
+                .map(t -> t.getLevel())
+                .orElse(0);
 
         return new MyPageProfileResponseDTO(
                 user.getNickname(),
                 user.getMajor(),
                 user.getGrade(),
-                teamLevel.getLevel(),
-                point.getBalance()
+                teamLevel,
+                point
         );
     }
 }
