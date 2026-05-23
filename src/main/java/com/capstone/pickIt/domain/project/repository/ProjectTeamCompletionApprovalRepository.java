b7942a19d28bf5abddca0c7c7c3b86b300f41004
ownue <enu_i@naver.com>
2026-05-23 15:20:24 +0900

    chore: DB 레벨 카운트 쿼리로 변경

--- src/main/java/com/capstone/pickIt/domain/project/repository/ProjectTeamCompletionApprovalRepository.java
+++ src/main/java/com/capstone/pickIt/domain/project/repository/ProjectTeamCompletionApprovalRepository.java
@@ -1,5 +1,6 @@
 package com.capstone.pickIt.domain.project.repository;
 
+import com.capstone.pickIt.domain.project.entity.CompletionDecision;
 import com.capstone.pickIt.domain.project.entity.ProjectTeamCompletionApproval;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -14,5 +15,10 @@ public interface ProjectTeamCompletionApprovalRepository
     Optional<ProjectTeamCompletionApproval> findByCompletionRequestIdAndUserId(
             Long completionRequestId,
             Long userId
+    );
+
+    long countByCompletionRequestIdAndDecision(
+            Long completionRequestId,
+            CompletionDecision decision
     );
 }
