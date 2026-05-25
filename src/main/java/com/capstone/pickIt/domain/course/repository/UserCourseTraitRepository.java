7b23ea216449f415f5a9d6fba1b267f62904a927
romdy <kimjh0937@gmail.com>
2026-05-25 20:17:33 +0900

    feat: UserCourseTraitRepository 다중 프로필 성향 배치 조회 쿼리 추가

--- src/main/java/com/capstone/pickIt/domain/course/repository/UserCourseTraitRepository.java
+++ src/main/java/com/capstone/pickIt/domain/course/repository/UserCourseTraitRepository.java
@@ -4,12 +4,20 @@ import com.capstone.pickIt.domain.course.entity.UserCourseTrait;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserCourseTraitRepository extends JpaRepository<UserCourseTrait, Long> {
 
     List<UserCourseTrait> findByUserCourseProfileId(Long userCourseProfileId);
+
+    @Query("""
+        SELECT uct FROM UserCourseTrait uct
+        JOIN FETCH uct.traitItem
+        WHERE uct.userCourseProfile.id IN :profileIds
+        """)
+    List<UserCourseTrait> findAllByUserCourseProfileIdIn(@Param("profileIds") List<Long> profileIds);
 
     @Modifying
     @Query("DELETE FROM UserCourseTrait uct WHERE uct.userCourseProfile.id = :userCourseProfileId")
